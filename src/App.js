7939e13f52840bde0007c68563eecc6f4a403952
ashjanraja <ashjannairat787@gmail.com>
2021-09-01 00:46:51 +0300

    up to date

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from "./logo.svg";
+import "./App.css";
+import * as UserCommponent from "./react-assignment";
+function App() {
+  return (
+    <div className="App">
+      <UserCommponent.compo1 />
+
+      <hr />
+
+      <UserCommponent.compo2 />
+
+      <hr />
+
+      <UserCommponent.compo3 />
+
+      <hr />
+      <UserCommponent.compo4 />
+
+      <hr />
+
+      <UserCommponent.compo5 />
+
+      <hr />
+     
+    </div>
+  );
+}
+
+export default App;
